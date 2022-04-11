576aef89569702f356e84cb6b159ab63bb1a0925
doniyorjonjaloliddinov <don2003.01.09@gmail.com>
2022-04-11 15:30:41 +0500

    add some changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
-
+// MONEY ASKER
 var price = 100;
 
 var money = parseInt(prompt("Qancha pulingiz bor"));
 if (money >= price) {
+    var pricePositiveAnswer = document.createElement("p");
+    pricePositiveAnswer.textContent = "Telefon sizniki";
+    document.body.append(pricePositiveAnswer);
     console.log("Telefon sizniki");
 }
 else {
+    var priceNegativeAnswer = document.createElement("p");
+    priceNegativeAnswer.textContent = "Oyligingiz chiqganda kelasiz";
+    document.body.append(priceNegativeAnswer);
     console.log("Oyligingiz chiqganda kelasiz");
 }
 
+
+// AGE ASKER
 var age = 16;
 var getAge = parseInt(prompt("Yoshingizni kiriting"));
 if (getAge >= age) {
+    var agePositiveAnswer = document.createElement("p");
+    agePositiveAnswer.textContent = "Siz musobaqada qatnashsangiz boladi";
+    document.body.append(agePositiveAnswer);
     console.log("Siz musobaqada qatnashsangiz boladi")
 }
 else {
+    var ageNegativeAnswer = document.createElement("p");
+    ageNegativeAnswer.textContent = "Xali kichkina ekansan sal katta bolib ol!";
+    document.body.append(ageNegativeAnswer);
     console.log("Xali kichkina ekansan sal katta bolib ol!")
 }
+
+
--- main.py
+++
@@ -1,9 +0,0 @@
-# name = "ism"
-
-# surname = ""  
-
-# def get_name(name):
-#     """Gets user name"""
-#     print()
-
-# print(name.upper())   
